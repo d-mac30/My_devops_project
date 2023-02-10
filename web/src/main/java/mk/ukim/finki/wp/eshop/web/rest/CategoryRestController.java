d6dd92339e2e108bdfc1793cd11bec1dc5cdf545
d-mac30 <100619082+d-mac30@users.noreply.github.com>
2023-02-11 00:39:12 +0100

    Update CategoryRestController.java

--- web/src/main/java/mk/ukim/finki/wp/eshop/web/rest/CategoryRestController.java
+++ web/src/main/java/mk/ukim/finki/wp/eshop/web/rest/CategoryRestController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/categories")
 public class CategoryRestController {
 
